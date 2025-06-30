bce6836e7d7854d10d5b4219c28702200d4d3689
SenLee1 <1786457344@qq.com>
2025-06-30 23:55:08 +0800

    Submitted

---
+++ Elevator/system/main.py
@@ -0,0 +1,19 @@
+import sys
+import threading
+from elevator_UI import ElevatorSystemUI
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication
+)
+if __name__ == '__main__':
+
+    QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
+    app = QApplication(sys.argv)
+    from elevator_system import ElevatorSystem
+    system = ElevatorSystem(num_elevators=2, max_floor=3, identity="Team8")
+    ui = ElevatorSystemUI(system, num_elevators=2, max_floors=3)
+    ui.show()
+    for t in system.threads:
+        t.start()
+    
+    sys.exit(app.exec())
